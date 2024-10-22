619cfaf504bba672af91596ad5bec548f2114f38
LeinneSW <nno88551@naver.com>
2024-10-22 22:46:40 +0900

    여기서 시작이 서서히 사라지도록 효과 추가

--- Assets/Scripts/StartHere.cs
+++ Assets/Scripts/StartHere.cs
@@ -1,17 +1,37 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StartHere : MonoBehaviour
 {
     public double DestroyTime = -1;
 
+    private SpriteRenderer spriteRenderer;
+    private const float FadeDuration = 0.25f; // 페이드 시간 (초 단위)
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void Update()
     {
-        if (DestroyTime > 0 && Time.timeAsDouble >= DestroyTime)
+        if (DestroyTime <= 0 || Time.timeAsDouble < DestroyTime) return;
+
+        DestroyTime = -1;
+        StartCoroutine(FadeOutSprite());
+    }
+
+    private IEnumerator FadeOutSprite()
+    {
+        var elapsedTime = 0f;
+        var color = spriteRenderer.color;
+        while (elapsedTime < FadeDuration)
         {
-            // TODO: 서서히 사라지도록 변경
-            Destroy(gameObject);
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Lerp(1f, 0f, elapsedTime / FadeDuration);
+            spriteRenderer.color = color;
+            yield return null;
         }
+        Destroy(gameObject);
     }
 }
